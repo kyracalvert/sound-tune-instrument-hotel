7b01c8a9d45a40b859281ccae05defbe87c5163a
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-08-23 11:40:14 -0500

    installed express and spun server.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT);
+})
