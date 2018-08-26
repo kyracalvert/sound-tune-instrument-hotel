e905998f670f954f26efa0a55b467a3f6b04f5ff
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-08-26 14:45:30 -0500

    updated sql table.

--- database.sql
+++ database.sql
@@ -2,14 +2,15 @@
 CREATE TABLE "owners" (
 "id" serial primary key,
 "name" varchar(120) not null,
+"last_name" varchar (120) not null,
 "email"varchar(120) not null,
 "phone" varchar(120) not null,
 "city" varchar(120) not null
 );
 
 /* Insert into owners table */
-INSERT INTO "owners" ("name", "email", "phone", "city")
-VALUES ('Khary Jackson', 'aliveness@music.com', '651-555-7847', 'St. Paul');
+INSERT INTO "owners" ("name", "last_name", "email", "phone", "city")
+VALUES ('Khary', 'Jackson', 'aliveness@music.com', '651-555-7847', 'St. Paul');
 
 
 
@@ -21,22 +22,26 @@ CREATE TABLE "instruments" (
 "model" varchar(120) not null,
 "description" varchar(120) not null,
 "issue" varchar(150) not null,
+"checked_in" BOOLEAN DEFAULT FALSE not null,
 "owner_id" INT REFERENCES "owners"
 );
 
 /* Insert into instruments table */
-INSERT INTO "instruments" ("ticket_date", "instrument", "model", "description", "issue", "owner_id")
-VALUES ('08-23-18', 'Guitar', 'Gibson Flying V', 'Mahogany with gold strings', 'severely out of tune', 1);
+INSERT INTO "instruments" ("ticket_date", "instrument", "model", "description", "issue", "checked_in", "owner_id")
+VALUES ('08-23-18', 'Guitar', 'Gibson Flying V', 'Mahogany with gold strings', 'severely out of tune', 'false', 1);
 
 
 
 /* Joins the instruments and owners to display name, tix date, instrument, model, description, and issue */
-SELECT "name", "ticket_date", "instrument", "model", "description", "issue"
+SELECT "name", "ticket_date", "instrument", "model", "description", "issue", "checked_in", "owner_id", "instruments"."id"
 FROM "owners" 
-JOIN "instruments" ON "owners"."id" = "instruments"."owner_id";
+FULL JOIN "instruments" ON "owners"."id" = "instruments"."owner_id";
+
 
 /* Number of instruments per owner */
 SELECT "name", COUNT ("instruments"."id")
 FROM "owners"
 FULL JOIN "instruments" ON "owners"."id" = "instruments"."owner_id"
 GROUP BY "owners"."name";
+
+/* DELETE empty rows from "instruments" table */
